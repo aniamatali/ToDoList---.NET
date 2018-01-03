70106dfe3ac87bedc3211ee3fc8342c48693ae23
Guest User <Guest@epicodus-79.local>
2018-01-03 15:51:09 -0800

    creates migrations up

--- ToDoList/Migrations/20180103230752_MakeTableNamesPlural.cs
+++ ToDoList/Migrations/20180103230752_MakeTableNamesPlural.cs
@@ -7,9 +7,22 @@ namespace ToDoList.Migrations
     public partial class MakeTableNamesPlural : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
-        {
-
-        }
+		{
+			migrationBuilder.DropForeignKey(name: "FK_Item_Category_CategoryId", table: "Item");
+			migrationBuilder.RenameTable(
+				name: "Item",
+				newName: "Items");
+			migrationBuilder.RenameTable(
+				name: "Category",
+				newName: "Categories");
+			migrationBuilder.AddForeignKey(
+				name: "FK_Item_Category_CategoryId",
+				table: "Items",
+				column: "CategoryId",
+				principalTable: "Categories",
+				principalColumn: "CategoryId",
+				onDelete: ReferentialAction.Cascade);
+		}
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
