27406826b8affe0dd8240f2d4fe5f259cea38a94
Guest User <Guest@epicodus-3.local>
2018-01-02 16:29:13 -0800

    creates todolist dotnet

---
+++ ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ToDoList.Controllers
+{
+	public class ItemsController : Controller
+	{
+		private ToDoListContext db = new ToDoListContext();
+		public IActionResult Index()
+		{
+			List<Item> model = db.Items.ToList();
+			return View(model);
+		}
+	}
+}
